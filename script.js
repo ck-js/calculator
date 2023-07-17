31a4797330dba2fe87de013d4eb25d89b94b16ed
Cliffton Kaewkarn <ck@Clifftons-MacBook-Air.local>
2023-07-17 18:06:08 +0700

    Add main layout and positioning of calculator. Add calculator operation functions and variables.

--- script.js
+++ script.js
@@ -1,2 +1,35 @@
-const calculatorContainer = document.querySelector('.calculator-container')
+let firstNumber = 0;
+let secondNumber = 0;
+let operator;
 
+
+
+function add(...args) {
+    let additionArray = args;
+    const addition = additionArray.reduce((sum, current) => 
+         sum + current);
+        return `Total: ${addition}`
+}
+// console.log(add(25,25,25,25));
+
+function subtract(a,  b) {
+const subtraction = a - b
+    console.log(subtraction);
+    return subtraction
+}
+// subtract(100,25)
+
+function multiply(a, b) {
+    const multiplication = a * b;
+    return multiplication;
+}
+
+function divide(a, b) {
+    const division = a / b;
+    return division;
+}
+
+function operate(a,b, operator, fn) {
+let result = fn(a + operator + b)
+return result;
+}
